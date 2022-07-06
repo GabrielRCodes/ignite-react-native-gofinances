112ce255a93c7aadbf3abd812a695d73bde0bd4e
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-06 12:18:16 -0300

    Adding loading from expo

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,11 +4,11 @@ export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.secondary};
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
-  color: purple;
+  color: ${({theme}) => theme.colors.title};
   font-size: 24px;
   font-weight: bold;
 `
