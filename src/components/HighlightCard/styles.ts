bd80184ba1e760219a1b87093a8ee9e80000f7a4
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-08 09:54:22 -0300

    Adding transactions cards

---
+++ src/components/HighlightCard/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons"
+import { AnyStyledComponent } from "styled-components";
+
+export const Container = styled.View`
+
+`;
+
+export const Header = styled.View`
+
+`;
+
+export const Title = styled.Text`
+
+`;
+
+export const Icon = styled(Feather as unknown as AnyStyledComponent)`
+
+`;
+
+export const Footer = styled.View`
+
+`;
+
+export const Amount = styled.Text`
+
+`;
+
+export const LastTransaction = styled.Text`
+
+`;
