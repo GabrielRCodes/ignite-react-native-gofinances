ddae4d018ae8e2b290397235c6c2c06d5a5d8648
GabrielRCodes <gabrielrcodes@gmail.com>
2022-07-08 10:00:36 -0300

    Finishing card creation

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,13 +1,17 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize"
 import { Feather } from "@expo/vector-icons"
 import { AnyStyledComponent } from "styled-components";
 
 export const Container = styled.View`
-
+  background-color: ${({ theme }) => theme.colors.shape};
+  width: ${RFValue(300)}px;
+  border-radius: 5px;
+  padding: 19px 23px;
+  padding-bottom: ${RFValue(42)}px;
 `;
 
 export const Header = styled.View`
-
 `;
 
 export const Title = styled.Text`
@@ -29,3 +33,5 @@ export const Amount = styled.Text`
 export const LastTransaction = styled.Text`
 
 `;
+
+// Relembrando: padding = interno | margin = externo
